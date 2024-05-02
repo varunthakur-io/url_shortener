36ff8e67d0167e2a5a78e478e349fde2ff82ed34
Varun Thakur <varunkumar84150@gmail.com>
2024-05-02 19:07:54 +0530

    Define route for the home page.

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const urlController = require('../controllers/urlController');
+const urlController = require("../controllers/urlController");
+
+// Define route for the home page
+router.get("/", (req, res) => {
+  res.send("Welcome to the URL shortener service!");
+});
 
 // Route for shortening URL
-router.post('/shorten', urlController.shortenURL);
+router.post("/shorten", urlController.shortenURL);
 
 // Route for redirecting shortened URL
-router.get('/:shortURL', urlController.redirectURL);
+router.get("/:shortURL", urlController.redirectURL);
 
 module.exports = router;
