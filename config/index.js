5ea936f8bb038d8873764acaeb469023ffdb2fef
Varun Thakur <varunkumar84150@gmail.com>
2026-04-26 15:19:15 +0530

    feat(config): implement barrel file pattern for centralized configuration access

---
+++ config/index.js
@@ -0,0 +1,5 @@
+import { env, dbUrl } from "./env.js";
+import mongooseConnection from "./mongoose.js";
+import redisClient from "./redis.js";
+
+export { env as config, dbUrl, mongooseConnection, redisClient };
